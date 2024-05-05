6a345d7c887f6d0916459fe4a38370dae03487e8
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2024-05-05 12:55:42 +0200

    Update metrics code

--- drvi/utils/metrics.py
+++ drvi/utils/metrics.py
@@ -1,19 +1,18 @@
 import numpy as np
 import pandas as pd
 from scipy import stats
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import min_weight_full_bipartite_matching
 from sklearn.feature_selection import mutual_info_classif
 
 
-def nn_alignment_score_per_dim_per_binary_ct(var_continues, ct_binary):
-    order = var_continues.argsort()
-    ct_binary = ct_binary[order]
-    alignment = np.clip(
-        (np.sum((ct_binary[:-1] == 1) & (ct_binary[1:] == 1)) / (np.sum(ct_binary == 1) - 1))  # fraction of cells of this type that are next to a cell of the same type
-        -
-        (np.sum(ct_binary == 1) / ct_binary.shape[0]),  # cancel random neighbors when CT is frequent
-        0, None,
-    )
-    return alignment
+def most_similar_averaging_score(result_matrix):
+    return np.mean(np.max(result_matrix, axis=0))
+
+
+def latent_matching_score(result_matrix):
+    row_ind, col_ind = min_weight_full_bipartite_matching(csr_matrix(-result_matrix - 1e-10))
+    return result_matrix[row_ind, col_ind].sum() / result_matrix.shape[1]
 
 
 def nn_alignment_score_per_dim(var_continues, ct_cat_series):
@@ -25,7 +24,7 @@ def nn_alignment_score_per_dim(var_continues, ct_cat_series):
         -
         (np.sum(ct_01, axis=0) / ct_01.shape[0]),  # cancel random neighbors when CT is frequent
         0, None,
-    )
+    ) / (1 - (np.sum(ct_01, axis=0) / ct_01.shape[0]))
     return alignment
 
 
