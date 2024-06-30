b2e49ab23c4823131ee4fb231aabf4ab5836411c
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2024-06-30 21:06:40 +0200

    Improve interpretability code

--- drvi/utils/interpretation/__init__.py
+++ drvi/utils/interpretation/__init__.py
@@ -1,5 +1,7 @@
-from .latent_to_expression import (find_effective_vars, get_generative_output,
+from .latent_to_expression import (get_generative_output,
                                    iterate_and_make_effect_adata,
                                    make_effect_adata,
                                    make_generative_samples_to_inspect,
-                                   sort_and_filter_effect_adata)
+                                   sort_and_filter_effect_adata,
+                                   find_differential_vars,
+                                   mark_differential_vars,)
