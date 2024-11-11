717f2a45fe2c31f74514eca0a758410725d39e74
moinfar <moinfar.amirali@gmail.com>
2024-11-11 11:08:45 +0100

    Update version lookup

--- src/drvi/__init__.py
+++ src/drvi/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import version
 
 from . import model, nn_modules, scvi_tools_based, utils
 
-__version__ = version("drvi")
+__version__ = version("drvi-py")
 
 __all__ = [
     "__version__",
