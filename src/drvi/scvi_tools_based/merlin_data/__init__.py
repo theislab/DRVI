55bc0e40070725844c6596d909ce6fb18867ffb5
moinfar <moinfar.amirali@gmail.com>
2025-02-12 09:39:10 +0100

    Improve error communication when Merlin not installed

--- src/drvi/scvi_tools_based/merlin_data/__init__.py
+++ src/drvi/scvi_tools_based/merlin_data/__init__.py
@@ -1,7 +1,21 @@
 import importlib
-import logging
 
-logger = logging.getLogger(__name__)
+
+def get_placeholder(name, error_message, allow_init=False):
+    error_message = error_message + f" Cannot use '{name}'."
+
+    class ClassLevelGetAttrMeta(type):
+        def __getattr__(cls, name):
+            raise ImportError(error_message)
+
+    class LazyNonExistingModulePlaceholder(metaclass=ClassLevelGetAttrMeta):
+        def __init__(self):
+            if not allow_init:
+                raise ImportError(error_message)
+            super().__init__()
+
+    return LazyNonExistingModulePlaceholder
+
 
 if importlib.util.find_spec("merlin"):
     from . import fields
@@ -10,12 +24,13 @@ if importlib.util.find_spec("merlin"):
     from ._data_manager import MerlinDataManager
     from ._data_splitter import MerlinDataSplitter
 else:
-    fields = None
-    MerlinData = None
-    MerlinTransformedDataLoader = None
-    MerlinDataManager = None
-    MerlinDataSplitter = None
-    logger.warning("Merlin is not installed. To use merline dataloader please install it.")
+    error_msg = "Merlin is not installed. To use merline dataloader please install it."
+    fields = get_placeholder("fields", error_msg)
+    MerlinData = get_placeholder("MerlinData", error_msg)
+    MerlinTransformedDataLoader = get_placeholder("MerlinTransformedDataLoader", error_msg)
+    MerlinDataManager = get_placeholder("MerlinDataManager", error_msg)
+    MerlinDataSplitter = get_placeholder("MerlinDataSplitter", error_msg)
+
 
 __all__ = [
     "MerlinData",
