29926f4b21ed9e77d743a55b7723a81b6b959c02
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2025-06-08 21:10:04 +0200

    Make discrete_mutual_info_score accessible

--- src/drvi/utils/metrics/__init__.py
+++ src/drvi/utils/metrics/__init__.py
@@ -5,11 +5,13 @@ from ._pairwise import (
     local_mutual_info_score,
     nn_alignment_score,
     spearman_correlataion_score,
+    discrete_mutual_info_score,
 )
 
 __all__ = [
     "nn_alignment_score",
     "local_mutual_info_score",
+    "discrete_mutual_info_score",
     "global_dim_mutual_info_score",
     "spearman_correlataion_score",
     "most_similar_averaging_score",
