0ce4dcfd6b8d8aa0d6c2ab164a095cbb7dac8b16
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2025-11-17 13:24:21 +0100

    Allow loading models from before pypi release

--- src/drvi/scvi_tools_based/model/_drvi.py
+++ src/drvi/scvi_tools_based/model/_drvi.py
@@ -153,6 +153,37 @@ class DRVI(RNASeqMixin, VAEMixin, DRVIArchesMixin, UnsupervisedTrainingMixin, Ba
 
         logger.info("The model has been initialized")
 
+    @staticmethod
+    def _update_source_registry_for_existing_model(source_registry: dict[str, Any]) -> dict[str, Any]:
+        """Update the source registry for an existing model to the latest version if any updates are needed."""
+        from packaging.version import Version
+
+        source_registry_drvi_version = Version(source_registry.get("drvi_version", "0.1.0"))  # "0.1.0" for legacy code before pypi release
+        logger.info(f"The model is trained with DRVI version {source_registry_drvi_version}.")
+
+        while source_registry_drvi_version < Version(drvi.__version__):
+            if source_registry_drvi_version < Version("0.1.9"):
+                # No braking change up to 0.1.9
+                source_registry_drvi_version = Version("0.1.9")
+            elif source_registry_drvi_version == Version("0.1.9"):
+                # log the transfer
+                logger.info("Modifying model args from 0.1.9 to 0.1.10 (no user action required)")
+                logger.info("Adding empty batch key ...")
+                source_registry["setup_args"]["batch_key"] = None
+                source_registry["field_registries"]["batch"] = {
+                    "data_registry": {"attr_name": "obs", "attr_key": "_scvi_batch"},
+                    "state_registry": {"categorical_mapping": np.array([0]), "original_key": "_scvi_batch"},
+                    "summary_stats": {"n_batch": 1},
+                }
+                source_registry_drvi_version = Version("0.1.10")
+                logger.info("Done updating source registry from 0.1.9 to 0.1.10.")
+            else:
+                # No braking change yet!
+                source_registry_drvi_version = Version(drvi.__version__)
+        logger.info(f"Loading source in DRVI version {drvi.__version__}.")
+        
+        return source_registry
+
     @classmethod
     @setup_anndata_dsp.dedent
     def setup_anndata(
@@ -196,28 +227,8 @@ class DRVI(RNASeqMixin, VAEMixin, DRVIArchesMixin, UnsupervisedTrainingMixin, Ba
 
         # We may need to manupulate in case of version updates (only when loading a model).
         if "source_registry" in kwargs:
-            from packaging.version import Version
-
             source_registry = kwargs["source_registry"]
-            source_registry_drvi_version = Version(source_registry["drvi_version"])
-            while source_registry_drvi_version < Version(drvi.__version__):
-                if source_registry_drvi_version < Version("0.1.9"):
-                    # No braking change up to 0.1.9
-                    source_registry_drvi_version = Version("0.1.9")
-                elif source_registry_drvi_version == Version("0.1.9"):
-                    # log the transfer
-                    logger.info("Modifying model args from 0.1.9 to 0.1.10 (no user action required)")
-                    logger.info("Adding empty batch key ...")
-                    source_registry["setup_args"]["batch_key"] = None
-                    source_registry["field_registries"]["batch"] = {
-                        "data_registry": {"attr_name": "obs", "attr_key": "_scvi_batch"},
-                        "state_registry": {"categorical_mapping": np.array([0]), "original_key": "_scvi_batch"},
-                        "summary_stats": {"n_batch": 1},
-                    }
-                    source_registry_drvi_version = Version("0.1.10")
-                else:
-                    # No braking change yet!
-                    source_registry_drvi_version = Version(drvi.__version__)
+            source_registry = cls._update_source_registry_for_existing_model(source_registry)
             kwargs["source_registry"] = source_registry
 
         adata_manager.register_fields(adata, **kwargs)
