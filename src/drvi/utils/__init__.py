7ba05e2a4a10866a645335fe0af5532debce669f
moinfar <moinfar.amirali@gmail.com>
2026-07-08 15:27:09 +0200

    Add porting functionality for drvi-py to scvi-tools migration

--- src/drvi/utils/__init__.py
+++ src/drvi/utils/__init__.py
@@ -1,10 +1,12 @@
 from . import metrics, misc
 from . import plotting as pl
 from . import tools as tl
+from .porting import port_to_scvi_tools
 
 __all__ = [
     "tl",
     "pl",
     "metrics",
     "misc",
+    "port_to_scvi_tools",
 ]
